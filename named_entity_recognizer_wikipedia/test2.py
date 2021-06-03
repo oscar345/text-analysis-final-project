7d1a7657e89f9d1fa45b444982338b56dab75c57
Noor4399404 <noor.otjens@gmail.com>
2021-06-03 21:53:28 +0200

    added first lines of index character

--- named_entity_recognizer_wikipedia/test2.py
+++ named_entity_recognizer_wikipedia/test2.py
@@ -1,11 +1,20 @@
-import sys 
+import sys
+from nltk import word_tokenize, sent_tokenize, pos_tag
 
-def main(sys.argv):
+def main():
     
     f = open('document.pos', 'w+')
-    f.write(sys.argv[1])
+    input_text = sys.argv[1]
+    raw = input_text
+    input_text_split = input_text.split()
+    token = word_tokenize(raw)
+    word_tag = pos_tag(token)
+
+    for x, y in word_tag:
+        f.write(x+" "+y+"\n")
+        print(raw.index(x), raw.index(x[-1]),x, y)
     f.close()
     
-    
+
 if __name__ == "__main__":
-    main(argv)
+    main()
