7f2c91782748103368d4f91c64a6ca2ae34d4952
Noor4399404 <noor.otjens@gmail.com>
2021-06-06 21:59:57 +0200

    done without tokenizer.tokenize function

--- named_entity_recognizer_wikipedia/test2.py
+++ named_entity_recognizer_wikipedia/test2.py
@@ -16,17 +16,23 @@ def main():
 
     word_tag = pos_tag(token)
     for x, y in word_tag:
-        f.write(x+" "+y+"\n")
+
 
         for i in range(len(sent_token)):
             if x in sent_token[i]:
-                sentence_number = ((i+1)*1000 +
+                sentence_number = ((i+1)*1000 + \
                                    sent_tokens_word[i].index(x) + 1)
                 sent_tokens_word[i].pop(sent_tokens_word[i].index(x))
         print(raw.index(x), raw.index(x[-1]), sentence_number, x, y)
+        f.write(str(raw.index(x)))
+        f.write(' ')
+        f.write(str(raw.index(x[-1])))
+        f.write(' ')
+        f.write(str(sentence_number))
+        f.write(' ')
+        f.write(x+" "+y+"\n")
     f.close()
     # Maybe remove dublicates with set?
-
 
 if __name__ == "__main__":
     main()
