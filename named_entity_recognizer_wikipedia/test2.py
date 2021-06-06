cb5992ce8ae74ed42f9445d19a0b6b2522572db9
Noor4399404 <noor.otjens@gmail.com>
2021-06-06 21:21:50 +0200

    sometimes numbers can be the same> Th"is" "is"

--- named_entity_recognizer_wikipedia/test2.py
+++ named_entity_recognizer_wikipedia/test2.py
@@ -12,14 +12,18 @@ def main():
     sent_tokens_word = []
     for sentences in range(len(sent_token)):
         sent_tokens_word.append(word_tokenize(sent_token[sentences]))
-    print('TOkanized sentences: ', sent_tokens_word)
     
+    begin_list = []
+    end_list = []
     word_tag = pos_tag(token)
     for x, y in word_tag:
         f.write(x+" "+y+"\n")
+
         for i in range(len(sent_token)):
             if x in sent_token[i]:
                 sentence_number = ((i+1)*1000 + sent_tokens_word[i].index(x) + 1)
+                sent_tokens_word[i].pop(sent_tokens_word[i].index(x))
+                #print(sent_tokens_word)
         print(raw.index(x), raw.index(x[-1]), sentence_number, x, y)
     f.close()
     
