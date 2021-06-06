f377b28453028b36e0fe03185fcc717ba6061053
Noor4399404 <noor.otjens@gmail.com>
2021-06-06 20:38:26 +0200

    trying to add sentence numbers

--- named_entity_recognizer_wikipedia/test2.py
+++ named_entity_recognizer_wikipedia/test2.py
@@ -8,10 +8,17 @@ def main():
     raw = input_text
     input_text_split = input_text.split()
     token = word_tokenize(raw)
+    sent_token = sent_tokenize(raw)
+    print(sent_token)
+    
     word_tag = pos_tag(token)
-
+    
     for x, y in word_tag:
         f.write(x+" "+y+"\n")
+        for i in range(len(sent_token)):
+            if x in sent_token[i]:
+                print(i+100)
+                print(sent_tokenize.index(x))
         print(raw.index(x), raw.index(x[-1]),x, y)
     f.close()
     
