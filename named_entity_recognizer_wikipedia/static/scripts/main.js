84ea637c9ba962b4169f3951b068549acae3c581
oscar345 <o.zwagers@icloud.com>
2021-06-07 13:07:10 +0200

    changed javascript

--- named_entity_recognizer_wikipedia/static/scripts/main.js
+++ named_entity_recognizer_wikipedia/static/scripts/main.js
@@ -63,7 +63,7 @@ function getProgress() {
 }
 
 
-$(function () {
+$(document).on("ready", function () {
     switchInputMethod();
     showUploadedFilename("file-input");
     showUploadedFilename("test-file-input");
