8713bd558cb32e58b8b1846853c4713c7546999f
Noor4399404 <noor.otjens@gmail.com>
2021-06-07 13:21:05 +0200

    pycodestyle

--- named_entity_recognizer_wikipedia/named_entity_recognizer.py
+++ named_entity_recognizer_wikipedia/named_entity_recognizer.py
@@ -135,7 +135,6 @@ class NamedEntityRecognizer():
                 pos_file.append(f"0 0 {token_id} {token} {tag}")
                 token_num += 1
         self.pos_file = pos_file
-        
 
     def add_pos_file(self, pos_file):
         self.pos_file = pos_file.split("\n")
@@ -197,7 +196,7 @@ class NamedEntityRecognizer():
         cause wikipedia urls and named entity tags to be assigned to the
         wrong tokens.
         """
-        
+
         jar = './Core_NLP_files/stanford-corenlp-4.2.0.jar'
         model = './Core_NLP_files/ner-model.ser.gz'
 
@@ -224,7 +223,7 @@ class NamedEntityRecognizer():
                     # if will not sync with the original tokens, the
                     # program will stop working because of an uneven
                     # amount of items in the lists
-                    
+
                 ne_tag = self.named_entities[index][1]
                 del self.named_entities[index:index + ne_index]
                 self.named_entities.insert(index, (token, ne_tag))
@@ -533,16 +532,19 @@ class Wikifier():
         output_file = list()
         for index, (key, value) in enumerate(self.output.items()):
             if (value[2] == ""):
-                output_file.append(f"{key} {value[0]} {self.pos_tags_pos[index]}")
+                output_file.append(f"{key} {value[0]}
+                                   {self.pos_tags_pos[index]}")
             else:
-                output_file.append(f"{key} {value[0]} {self.pos_tags_pos[index]} {value[1]} {value[2]}")
+                output_file.append(f"{key} {value[0]}
+                                   {self.pos_tags_pos[index]}
+                                   {value[1]} {value[2]}")
         return ("\n").join(output_file)
 
     def create_dict_output(self):
-        
+
         for i, location in enumerate(self.token_positions):
             self.output[location] = [self.tokens[i], self.named_entities[i][1],
-                                self.wiki_urls[i]]
+                                     self.wiki_urls[i]]
         return self.output
 
 
